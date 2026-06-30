c3a73796aa24109afec5375c60aee8846d19087b
MDimas57 <stiyawandimas756@gmail.com>
2026-06-30 21:05:40 +0700

    commit14

--- app/Filament/Resources/CategoryBanners/Tables/CategoryBannersTable.php
+++ app/Filament/Resources/CategoryBanners/Tables/CategoryBannersTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Actions\DeleteAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
